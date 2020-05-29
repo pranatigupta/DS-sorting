cb5a78983a83ba95aa9c52440a6f2a37ab488cd6
pranatigupta <55241646+pranatigupta@users.noreply.github.com>
2020-05-29 17:15:59 +0530

    Add files via upload

---
+++ quick.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<time.h>
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+
+int part(int arr[],int lower,int upper){
+    int i = lower-1;
+    int pivot = arr[upper];
+    for (int j = lower; j < upper; j++)
+    {
+        if(pivot>arr[j]){              
+            i++;
+            swap(&arr[i],&arr[j]);
+        }
+    }
+    swap(&arr[i+1],&arr[upper]);
+    return i+1; 
+}
+
+
+void QuickSort(int arr[],int lower,int upper){
+    if(lower<upper){
+        int partition = part(arr,lower,upper);
+        QuickSort(arr,lower,partition-1);
+        QuickSort(arr,partition+1,upper);
+    }
+}
+
+int main(){
+    int arr[10000];
+    for (int i = 10000; i < 0; i--)
+    {
+        arr[i]=i;
+    }
+    clock_t t = clock();
+    QuickSort(arr,0,10000);
+    t=clock()-t;
+    long double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("Time Taken = %Lf\n",time_taken);
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
